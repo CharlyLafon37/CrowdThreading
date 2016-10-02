3ec2dab435d939180eea174e02d43718a5f86ffb
Charly Lafon <charlylafon@gmail.com>
2016-10-02 13:40:18 +0200

    Complete layout of obstacles

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <SDL2/SDL.h>
+#include <stdlib.h>
 #include <stdio.h>
+
+#define WINDOW_WIDTH 512
+#define WINDOW_HEIGHT 128
+
+void obstaclesLayout(SDL_Rect obstacles[]);
 
 int main(int argc, char** argv)
 {
@@ -8,45 +14,62 @@ int main(int argc, char** argv)
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
     
-    window = SDL_CreateWindow("CrowdThreading",
-                               SDL_WINDOWPOS_UNDEFINED,
-                               SDL_WINDOWPOS_UNDEFINED,
-                               640,
-                               480,
-                               SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    
-    if(!window)
-    {
-        fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
-        return -1;
-    }
-    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+    if (SDL_VideoInit(NULL) < 0)
     {
         fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
         return -1;
     }
     
+    window = SDL_CreateWindow("CrowdThreading",
+                               SDL_WINDOWPOS_CENTERED,
+                               SDL_WINDOWPOS_CENTERED,
+                               512,
+                               128,
+                               SDL_WINDOW_SHOWN);
+    if(!window)
+    {
+        fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+        return -1;
+    }
+    
+    renderer =  SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == NULL)
+    {
+        printf("Erreur lors de la creation d'un renderer : %s",SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    
     /****/
     
-    SDL_Rect rect;
-    rect.x = 50; rect.y = 50; rect.w = 50; rect.h = 50;
+    SDL_SetRenderDrawColor(renderer, 175, 175, 175, 255); // Background = grey
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Rectangles = black
     
-    SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
+    SDL_Rect obstacles[4];
     
-    SDL_RenderClear( renderer );
+    obstaclesLayout(obstacles);
     
-    SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+    // Render rect
+    SDL_RenderFillRects(renderer, obstacles, 4);
     
-    SDL_RenderDrawRect(renderer, &rect);
-    
+    // Render the rect to the screen
     SDL_RenderPresent(renderer);
     
-    SDL_Delay(10000); /* Attendre dix secondes, que l'utilisateur voie la fenêtre */
-            
+    SDL_Delay(5000); /* Attendre cinq secondes, que l'utilisateur voie la fenêtre */
+    
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     
     SDL_Quit();
     
     return 0;
+}
+
+void obstaclesLayout(SDL_Rect obstacles[])
+{
+    obstacles[0].w = 16; obstacles[0].h = 60; obstacles[0].x = 0; obstacles[0].y = 0;
+    obstacles[1].w = 16; obstacles[1].h = 60; obstacles[1].x = 0; obstacles[1].y = WINDOW_HEIGHT - obstacles[1].h;
+    obstacles[2].w = 16; obstacles[2].h = 56; obstacles[2].x = 128 - obstacles[2].w; obstacles[2].y = 0;
+    obstacles[3].w = 16; obstacles[3].h = 56; obstacles[3].x = 128 - obstacles[3].w; obstacles[3].y = WINDOW_HEIGHT - obstacles[3].h;
 }
