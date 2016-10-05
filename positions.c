0b14325a90ffc758801544c0749c263820b2489e
Charly Lafon <charlylafon@gmail.com>
2016-10-05 12:08:01 +0200

    change in posiitons.c

--- positions.c
+++ positions.c
@@ -26,8 +26,10 @@ void spawnPeople(SDL_Rect people[], int nbPeople) // MISSING : PEOPLE SPAWNING O
 
     // Initialisation du plateau pour verifier la position des personnes.
     int plateau[WINDOW_WIDTH][WINDOW_HEIGHT];
-    for(i=0;i<WINDOW_WIDTH;i++){
-        for(j=0;j<WINDOW_HEIGHT;j++){
+    for(i=0;i<WINDOW_WIDTH;i++)
+    {
+        for(j=0;j<WINDOW_HEIGHT;j++)
+        {
             plateau[i][j]=0;
         }
     }
@@ -36,20 +38,24 @@ void spawnPeople(SDL_Rect people[], int nbPeople) // MISSING : PEOPLE SPAWNING O
     {
         people[i].w = PEOPLE_WIDTH;
         people[i].h = PEOPLE_HEIGHT;
-	int randX=rand()%(XMAX_PEOPLE-XMIN_PEOPLE) + XMIN_PEOPLE;
-	int randY=rand()%(YMAX_PEOPLE-YMIN_PEOPLE) + YMIN_PEOPLE;
+        int randX=rand()%(XMAX_PEOPLE-XMIN_PEOPLE) + XMIN_PEOPLE;
+        int randY=rand()%(YMAX_PEOPLE-YMIN_PEOPLE) + YMIN_PEOPLE;
 
-	while(plateau[randX][randY]==1 || plateau[randX+PEOPLE_WIDTH-1][randY+PEOPLE_HEIGHT-1]==1
-	     ||plateau[randX][randY+PEOPLE_HEIGHT-1]==1 || plateau[randX+PEOPLE_WIDTH-1][randY]==1){ 
-	    randX=rand()%(XMAX_PEOPLE-XMIN_PEOPLE) + XMIN_PEOPLE;
-	    randY=rand()%(YMAX_PEOPLE-YMIN_PEOPLE) + YMIN_PEOPLE;
+        while(plateau[randX][randY]==1 || plateau[randX+PEOPLE_WIDTH-1][randY+PEOPLE_HEIGHT-1]==1
+              ||plateau[randX][randY+PEOPLE_HEIGHT-1]==1 || plateau[randX+PEOPLE_WIDTH-1][randY]==1)
+        {
+            randX=rand()%(XMAX_PEOPLE-XMIN_PEOPLE) + XMIN_PEOPLE;
+            randY=rand()%(YMAX_PEOPLE-YMIN_PEOPLE) + YMIN_PEOPLE;
         }
         people[i].x = randX;
         people[i].y = randY;
-	for(j=randX;j<randX+PEOPLE_WIDTH;j++){
-            for(k=randY;k<randY+PEOPLE_HEIGHT;k++){
+        
+        for(j=randX;j<randX+PEOPLE_WIDTH;j++)
+        {
+            for(k=randY;k<randY+PEOPLE_HEIGHT;k++)
+            {
                 plateau[j][k]=1;
             }
-	}
+        }
     }
 }
