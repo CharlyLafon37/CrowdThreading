4ca20445dcfd1625745b2d07b397240505849f2b
Monzein-Stocky <thomas.monzein-stocky@etu.unice.fr>
2016-10-06 15:19:08 +0200

    1ere version de -t2

--- main.c
+++ main.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <pthread.h>
 #include "positions.h"
 #include "utils.h"
 
 void argumentsTreatment(char** argv, int nbArguments, int* nbPeople);
 void print(SDL_Renderer* renderer, SDL_Rect obstacles[], SDL_Rect people[], int nbPeople);
 
+typedef struct thread_person_data thread_person_data;
+struct thread_person_data
+{
+	int n;
+	int nbPeople;
+	SDL_Rect* people;
+};
+
+void spawnPeopleThread(SDL_Rect people[], int nbPeople);
+void *thread_person(thread_person_data *arg);
+
 int option_thread=0;
+int iterations=500;
+
 
 int main(int argc, char** argv)
 {
@@ -52,25 +66,43 @@ int main(int argc, char** argv)
     SDL_Rect people[nbPeople];
     
     obstaclesLayout(obstacles);
-    spawnPeople(people, nbPeople);
-    
-    print(renderer, obstacles, people, nbPeople);
-    
-    /****/
-    int i, j;
-    for(i = 0; i < 250; i++)
-    {
-        for(j = 0; j < nbPeople; j++)
-        {
-            SDL_Point newPosition = move_people(j, people, nbPeople, XAZIMUTH, YAZIMUTH);
-            
-            people[j].x = newPosition.x;
-            people[j].y = newPosition.y;
-        }
-        print(renderer, obstacles, people, nbPeople);
-        SDL_Delay(50);
+
+    // Si on simule sur 1 seul thread.
+    if(option_thread==0){
+	    spawnPeople(people, nbPeople);
+	    
+	    print(renderer, obstacles, people, nbPeople);
+	    
+	    /****/
+	    int i, j;
+	    for(i = 0; i < iterations; i++)
+	    {
+		for(j = 0; j < nbPeople; j++)
+		{
+		    SDL_Point newPosition = move_people(j, people, nbPeople, XAZIMUTH, YAZIMUTH);
+		    
+		    people[j].x = newPosition.x;
+		    people[j].y = newPosition.y;
+			SDL_Delay(5);
+		}
+		print(renderer, obstacles, people, nbPeople);
+		//SDL_Delay(50);
+	    }
+    }// Si on simule en divisant le terrain en 4 thread.
+    else if(option_thread==1){
+		
+    }// Si on simule avec un thread par personne.
+    else if(option_thread==2){
+		// Création des personnes et de leur thread.
+		spawnPeopleThread(people,nbPeople);
+		// Rendu graphique
+		int i=0;
+		for(i=0;i<2*iterations;i++){
+			print(renderer, obstacles, people, nbPeople);
+		}
+		
     }
-    
+
     SDL_Delay(5000); // Attendre cinq secondes, que l'utilisateur voie la fenêtre
     
     SDL_DestroyRenderer(renderer);
@@ -122,3 +154,85 @@ void print(SDL_Renderer* renderer, SDL_Rect obstacles[], SDL_Rect people[], int 
     
     SDL_RenderPresent(renderer); // Rendering on screen
 }
+
+
+void spawnPeopleThread(SDL_Rect people[], int nbPeople)
+{
+    int i,j,k;
+    srand(time(NULL));
+
+    // Initialisation du plateau pour verifier la position des personnes.
+    int plateau[WINDOW_WIDTH][WINDOW_HEIGHT];
+    for(i=0;i<WINDOW_WIDTH;i++)
+    {
+        for(j=0;j<WINDOW_HEIGHT;j++)
+        {
+            plateau[i][j]=0;
+        }
+    }
+   
+	pthread_t threads[nbPeople];
+	thread_person_data datas[nbPeople];
+
+    for(i = 0; i < nbPeople; i++)
+    {
+        people[i].w = PEOPLE_WIDTH;
+        people[i].h = PEOPLE_HEIGHT;
+        int randX=rand()%(XMAX_PEOPLE-XMIN_PEOPLE) + XMIN_PEOPLE;
+        int randY=rand()%(YMAX_PEOPLE-YMIN_PEOPLE) + YMIN_PEOPLE;
+
+        while(plateau[randX][randY]==1 || plateau[randX+PEOPLE_WIDTH-1][randY+PEOPLE_HEIGHT-1]==1
+              ||plateau[randX][randY+PEOPLE_HEIGHT-1]==1 || plateau[randX+PEOPLE_WIDTH-1][randY]==1)
+        {
+            randX=rand()%(XMAX_PEOPLE-XMIN_PEOPLE) + XMIN_PEOPLE;
+            randY=rand()%(YMAX_PEOPLE-YMIN_PEOPLE) + YMIN_PEOPLE;
+        }
+        people[i].x = randX;
+        people[i].y = randY;
+        
+        for(j=randX;j<randX+PEOPLE_WIDTH;j++)
+        {
+            for(k=randY;k<randY+PEOPLE_HEIGHT;k++)
+            {
+                plateau[j][k]=1;
+            }
+        }
+		// Création du thread de la personne.
+		datas[i].n=i;
+		datas[i].nbPeople=nbPeople;
+		datas[i].people=people;
+		printf("creation du thread %d\n",i);
+		if(pthread_create(&threads[i],NULL,thread_person,&datas[i])==-1){
+			perror("pthread_create");
+			return;
+		}
+		if(pthread_join(threads[i],NULL)){
+			perror("pthread_join");
+			return;
+		}
+		printf("apres creation du thread %d\n",i);
+		
+		
+    }
+}
+
+void *thread_person(thread_person_data *arg){
+
+	int i=0;
+	//(thread_person_data) arg;
+	printf("Debut du thread de la personne %d\n",arg->n);
+
+	for(i=0;i<iterations;i++){
+		SDL_Point newPosition = move_people(arg->n, arg->people, arg->nbPeople, XAZIMUTH, YAZIMUTH);
+				
+		arg->people[arg->n].x = newPosition.x;
+		arg->people[arg->n].y = newPosition.y;
+		SDL_Delay(20);
+	}
+	pthread_exit(NULL);
+
+}
+
+
+
+
