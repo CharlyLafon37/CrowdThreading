199feea02282fe9f08c01fc9bf783e37d64248b1
Monzein-Stocky <thomas.monzein-stocky@etu.unice.fr>
2016-10-07 19:37:17 +0200

    Working -t1. Graphique et pas de passage d'un thread à l'autre.

--- compile.sh
+++ compile.sh
@@ -6,5 +6,5 @@
 gcc main.c positions.c utils.c thread.c -lSDL2 -lSDL2main -lm -lpthread -o bin/crow.out
 
 # Tests
-gcc -o bin/testUtils.out utils.c testUtils.c positions.c -lSDL2 -lSDL2main
+#gcc -o bin/testUtils.out utils.c testUtils.c positions.c -lSDL2 -lSDL2main
 
--- thread.h
+++ thread.h
@@ -7,6 +7,8 @@
 
 #include "main.h"
 
+#define NB_THREAD 4
+
 typedef struct thread_person_data thread_person_data;
 struct thread_person_data
 {
@@ -16,7 +18,21 @@ struct thread_person_data
     Person* people;
 };
 
+typedef struct thread_space_data thread_space_data;
+struct thread_space_data
+{
+    int n;
+    int *restant;
+    int nbPeople;
+    Person* people;
+	int nbPeopleSpace;
+	int* peopleSpace;
+};
+
 void spawnPeopleThread(Person people[], int nbPeople, int *restant);
 void *thread_person(thread_person_data *arg);
+
+void spawnPeopleThreadSpace(Person people[], int nbPeople, int *restant);
+void *thread_space(thread_space_data *arg);
 
 #endif
